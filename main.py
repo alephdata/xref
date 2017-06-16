38b812cadb13abdba0ae66c0676849fd3128884e
rhiaro <amy@rhiaro.co.uk>
2017-06-16 17:12:36 +0200

    Commandline args, better output

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from key import aleph_key
+import sys
 import requests
 import json
 import csv
 from tabulate import tabulate
+from key import aleph_key
 
 
 def get_search_terms(csv_fn, columns, delimiter=',', quotechar='"'):
@@ -10,21 +11,27 @@ def get_search_terms(csv_fn, columns, delimiter=',', quotechar='"'):
     Takes a csv file and a list of the names of the columns to use 
     for crossreferencing 
     """
-    with open(csv_fn, 'rb') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=delimiter, quotechar=quotechar)
-        # Assume the first row has the column names.
-        colnames = next(csvreader)
-        usecols = []
-        search_terms = []
-        for column in columns:
-            try:
-                usecols.append(colnames.index(column))
-            except ValueError:
-                pass
-        for row in csvreader:
-            for i in usecols:
-                search_terms.append(row[i])
-    return set(search_terms)
+    try:
+        with open(csv_fn, 'rb') as csvfile:
+            csvreader = csv.reader(
+                csvfile, delimiter=delimiter, quotechar=quotechar)
+            # Assume the first row has the column names.
+            colnames = next(csvreader)
+            usecols = []
+            search_terms = []
+            for column in columns:
+                try:
+                    usecols.append(colnames.index(column))
+                except ValueError:
+                    print "Column '%s' was not found in the input." % column
+            for row in csvreader:
+                for i in usecols:
+                    search_terms.append(row[i])
+        print "%s unique search terms found.." % len(search_terms)
+        return set(search_terms)
+    except IOError:
+        print "File not found. More information: https://http.cat/404"
+        return []
 
 
 def api_req(req, params={}, results=[]):
@@ -53,15 +60,17 @@ def search_term(term):
     """ Find entities matching string """
 
     print "Searching ... %s ..." % term
+    meta = None
 
     docs_with_term = get_search_docs(term)
-    meta = "%s: %s documents with this term" % (
-        term, len(docs_with_term))
 
     req = "api/1/entities"
     par = {"q": term, "limit": 1000}
     r = api_req(req, par, [])
-    meta += "; %s entities found:" % len(r)
+
+    if len(docs_with_term) > 0 or len(r) > 0:
+        meta = "%s: %s documents with this term; %s entities found" % (
+            term, len(docs_with_term), len(r))
 
     print "Found %s documents and %s entities." % (len(docs_with_term), len(r))
 
@@ -69,22 +78,23 @@ def search_term(term):
 
 
 def aggregate_results(name, results):
+    if len(results) > 0:
+        out = {"Entity": [], "Name": [], "Source": [], "Documents": []}
 
-    out = {"Entity": [], "Name": [], "Source": [], "Documents": []}
+        for res in results:
+            docs = []
+            out["Name"].append(res["name"])
+            out["Entity"].append(res["id"])
+            if "dataset" in res:
+                source = "datsets/%s" % res["dataset"]
+            elif "collection_id" in res:
+                source = "collections/%s" % res["collection_id"]
+                docs = get_entity_docs(res["id"])
+            out["Documents"].append(len(docs) or "")
+            out["Source"].append(source)
 
-    for res in results:
-        docs = []
-        out["Name"].append(res["name"])
-        out["Entity"].append(res["id"])
-        if "dataset" in res:
-            source = "datsets/%s" % res["dataset"]
-        elif "collection_id" in res:
-            source = "collections/%s" % res["collection_id"]
-            docs = get_entity_docs(res["id"])
-        out["Documents"].append(len(docs) or "")
-        out["Source"].append(source)
-
-    return out
+        return out
+    return {}
 
 
 def get_entity_docs(entity_id):
@@ -106,20 +116,25 @@ def get_search_docs(term):
 
 
 def make_nice(search_meta, results):
-    table = search_meta + "\n"
+    table = ""
+    if search_meta is not None:
+        table = "%s \n" % search_meta
     table += tabulate(results, headers="keys", tablefmt="psql")
+    table += "\n\n"
     return table.encode('utf8')
 
-# TODO: enter search/filename at cl
+
+def run(filename, *column_names):
+    terms = get_search_terms(filename, column_names[0])
+    with open('out', 'w') as f:
+        for term in terms:
+            r = search_term(term)
+            f.write(make_nice(r["search_meta"], r["results"]))
+
+    print "Result output to file: `out`"
+
+if __name__ == "__main__":
+    run(sys.argv[1], sys.argv[2:])
+
 # TODO: option to list documents with search term
 # TODO: option to list documents for found entities
-
-# r = search_term("Vladimir Putin")
-# r = search_term("Levan Vasadze")
-
-terms = get_search_terms('../docs/sample.csv', ["Name", "asdf"])
-with open('out', 'w') as f:
-    for term in terms:
-        r = search_term(term)
-        f.write(make_nice(r["search_meta"], r["results"]))
-
