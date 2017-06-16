50608c39bb027c9e6b48258cfa5ba31bb6486ed1
rhiaro <amy@rhiaro.co.uk>
2017-06-16 16:07:44 +0200

    Search specific columns in spreadsheet

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 from key import aleph_key
 import requests
 import json
+import csv
 from tabulate import tabulate
+
+
+def get_search_terms(csv_fn, columns, delimiter=',', quotechar='"'):
+    """ 
+    Takes a csv file and a list of the names of the columns to use 
+    for crossreferencing 
+    """
+    with open(csv_fn, 'rb') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=delimiter, quotechar=quotechar)
+        # Assume the first row has the column names.
+        colnames = next(csvreader)
+        usecols = []
+        search_terms = []
+        for column in columns:
+            try:
+                usecols.append(colnames.index(column))
+            except ValueError:
+                pass
+        for row in csvreader:
+            for i in usecols:
+                search_terms.append(row[i])
+    return search_terms
 
 
 def api_req(req, params={}, results=[]):
@@ -29,14 +52,19 @@ def api_req(req, params={}, results=[]):
 def search_term(term):
     """ Find entities matching string """
 
+    print "Searching ... %s ..." % term
+
     docs_with_term = get_search_docs(term)
     meta = "%s: %s documents with this term" % (
-            term, len(docs_with_term))
+        term, len(docs_with_term))
 
     req = "api/1/entities"
     par = {"q": term, "limit": 1000}
     r = api_req(req, par, [])
     meta += "; %s entities found:" % len(r)
+
+    print "Found %s documents and %s entities." % (len(docs_with_term), len(r))
+
     return {"search_meta": meta, "results": aggregate_results(term, r)}
 
 
@@ -82,8 +110,16 @@ def make_nice(search_meta, results):
     table += tabulate(results, headers="keys", tablefmt="psql")
     return table.encode('utf8')
 
-# TODO: enter search at cl
+# TODO: enter search/filename at cl
+# TODO: option to list documents with search term
+# TODO: option to list documents for found entities
+
 # r = search_term("Vladimir Putin")
-r = search_term("Levan Vasadze")
+# r = search_term("Levan Vasadze")
+
+terms = get_search_terms('../docs/sample.csv', ["Name", "asdf"])
 with open('out', 'w') as f:
-    f.write(make_nice(r["search_meta"], r["results"]))
+    for term in terms:
+        r = search_term(term)
+        f.write(make_nice(r["search_meta"], r["results"]))
+
